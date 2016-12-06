a2f36058a08587c65ca7f6927fb3eeac0fc7ab94
Erica Lee <erica.lee@students.olin.edu>
2016-12-06 15:59:02 -0500

    FIX: Buzzing is now static-y shriek-y noise with different pitches

--- microSD/microSD/microSD.ino
+++ microSD/microSD/microSD.ino
@@ -2,22 +2,25 @@
 #define SD_ChipSelectPin 10
 #include <TMRpcm.h>           //  also need to include this library...
 
-TMRpcm tmrpcm;   // create an object for use in this sketch
-
+TMRpcm sound;   // create an object for use in this sketch
 void setup(){
 
-  tmrpcm.speakerPin = 9; //11 on Mega, 9 on Uno, Nano, etc
-
+  sound.speakerPin = 9; //11 on Mega, 9 on Uno, Nano, etc
+  sound.setVolume(5);
+  sound.quality(0);
+  
   Serial.begin(9600);
   if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
     Serial.println("SD fail");  
     return;   // don't do anything more if not
   }
+  sound.play("out.wav");
+  Serial.println(sound.isPlaying());  
 }
 
 void loop(){ 
-
-   tmrpcm.play("POESound.wav");
-
-
+//  soundTrack = SD.open("POESound.wav");
+//  Serial.println(soundTrack.read());
+    
+    
 }
